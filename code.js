5ed9cd0a7d77c0f79e6531c77e795ba8097745ff
Mecopi <mecopilive@gmail.com>
2023-09-27 19:40:54 +0200

    rework project to make it js

---
+++ code.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName('ytd-playlist-video-renderer')[0].getElementsByTagName('div')[20].getElementsByTagName('yt-icon-button')[0].click(); //Click on dropdown
+
+document.getElementsByTagName('tp-yt-paper-listbox')[1].getElementsByTagName('yt-formatted-string')[6].click() //click on
