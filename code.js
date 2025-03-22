d61507f3a698287b46cc4e2ae921c57b287771a6
benjGam <mecopilive@gmail.com>
2025-03-22 11:13:05 +0100

    fix: rework script on 'clickDelete' function to keep code working

--- code.js
+++ code.js
@@ -1,19 +1,24 @@
 let x = 0;
 
-setInterval(main(), 500);
+setInterval(() => main(), 30);
 
 function main() {
   clickDropdown();
-  setTimeout(clickDelete(), 20);  
+  setTimeout(clickDelete(), 20);
   x++;
 }
 
 function clickDropdown() {
-  const dropdown = document.getElementsByTagName('ytd-playlist-video-renderer')[x].getElementsByTagName('button')[0];
-  dropdown.click(); 
+  console.log(document.getElementsByTagName("ytd-playlist-video-renderer")[x]);
+  const dropdown = document
+    .getElementsByTagName("ytd-playlist-video-renderer")
+    [x].getElementsByTagName("button")[0];
+  dropdown.click();
 }
 
 function clickDelete() {
-  const deleteLabel = document.getElementsByTagName('tp-yt-paper-listbox')[1].getElementsByTagName('yt-formatted-string')[6];
-  deleteLabel.click();
+  const label = document
+    .getElementsByTagName("tp-yt-paper-listbox")[1]
+    .getElementsByTagName("yt-formatted-string")[6];
+  label.click();
 }
